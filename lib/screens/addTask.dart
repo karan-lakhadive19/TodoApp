1b4c582c6fa2e1211b14a8be50c3d39ad231abbc
Karan Lakhadive <lakhadive1926@gmail.com>
2023-05-03 13:18:49 +0530

    Data retrieved from firestoe and display on Home screen

--- lib/screens/addTask.dart
+++ lib/screens/addTask.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -11,6 +13,38 @@ class AddTask extends StatefulWidget {
 }
 
 class _AddTaskState extends State<AddTask> {
+
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descController = TextEditingController();
+
+  addTaskFirebase()async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    // UserCredential userCredential = await FirebaseAuth.instance;
+    final User? user = auth.currentUser;
+    String uid = user!.uid;
+    var time = DateTime.now();
+    await FirebaseFirestore.instance.collection('task').doc(uid).collection('MyTask').doc(time.toString()).set({
+      'title':titleController.text,
+      'desc':descController.text,
+      'time':time.toString()
+    });
+    const snackBar = SnackBar(
+        content: Text(
+          "Task Added!",
+          style: TextStyle(
+            fontSize: 16, // adjust to your desired font size
+            color: Colors.white, // set text color
+          ),
+        ),
+        backgroundColor: Colors.blue, // set background color
+        behavior: SnackBarBehavior.floating, // set the behavior to floating
+      );
+       
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     final maxLines = 5;
@@ -25,6 +59,7 @@ class _AddTaskState extends State<AddTask> {
             children: [
               Container(
                 child: TextField(
+                  controller: titleController,
                   decoration: InputDecoration(
                       hintText: "Enter Title", border: OutlineInputBorder()),
                 ),
@@ -35,6 +70,7 @@ class _AddTaskState extends State<AddTask> {
               Container(
                  height: maxLines * 24.0,
                 child: TextField(
+                  controller: descController,
                   maxLines: maxLines,
                   decoration: InputDecoration(
                       hintText: "Enter Description",
@@ -48,7 +84,9 @@ class _AddTaskState extends State<AddTask> {
                 width: double.infinity,
                 height: 45,
                 child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      addTaskFirebase();
+                    },
                     child: Text("Add Task",
                         style: GoogleFonts.roboto(
                             fontSize: 20, fontWeight: FontWeight.bold))),
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,8 +4,31 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_app/screens/addTask.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String uid = '';
+  @override
+  void initState() {
+    // TODO: implement initState
+    getUid();
+    super.initState();
+  }
+
+  // Code to get user id 
+
+  getUid() async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    final User? user = auth.currentUser;
+    setState(() {
+      uid = user!.uid;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,9 +44,49 @@ class HomeScreen extends StatelessWidget {
         ],
       ),
       body: Container(
+        padding: EdgeInsets.all(10),
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        color: Colors.red,
+
+// Retrieving data from firestore
+
+
+        child: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('task')
+              .doc(uid)
+              .collection('MyTask')
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              final docu = snapshot.data!.docs;
+              return ListView.builder(
+                itemCount: docu.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: EdgeInsets.only(bottom: 10),
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10)),
+                    height: 90,
+                    child: Column(
+                      children: [
+                        Text(docu[index]['title']),
+                      ],
+                    ),
+                  );
+                },
+              );
+            }
+          },
+        ),
+
+
+
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(
